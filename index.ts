7679905890ee6ff36392d4d77cc4ce0128561e4b
Ignatius Bagus <ignatius.mbs@gmail.com>
2023-08-10 11:28:56 +0700

    fix: ensure forward slashes in src attributes (#4)

--- index.ts
+++ index.ts
@@ -47,7 +47,7 @@ export default function ({
 						)}`;
 						const file = pathname.slice(pages.length + 1, -'.html'.length);
 						fs.writeFileSync(join(assets, `./${file}.js`), content);
-						return `<script src="./${file}.js"></script>`;
+						return `<script src="./${file.replace(/\\/g, '/')}.js"></script>`;
 					},
 				);
 				fs.writeFileSync(pathname, externalized);
